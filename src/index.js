1178ff496b2216198288f72fab9927889065a6da
Sam Benning <bensam1993@gmail.com>
2021-12-09 20:19:33 -0500

    Followed asset management webpack tutorial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import myName from './myName';
+import Icon from './icon.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
 
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = myName('Sam');
+    element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
 
     return element;
 }
